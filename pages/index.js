041d77baf4f2835b7a84459240ac7c98d07be143
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-06-09 16:35:44 +0530

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,13 @@
 import Head from "next/head";
-import Landing from "../components/Landing";
-import Transaction from "../components/Transaction";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>FCL Quickstart with NextJS</title>
+        <title>FlowPay</title>
         <meta name="description" content="My first web3 app on Flow!" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-
-      <main>
-        <Transaction />
-        <div className="grid">
-          <Landing />
-        </div>
-      </main>
     </div>
   );
 }
