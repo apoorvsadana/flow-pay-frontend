becf4ed071933497e671fccf0acb8cfa4ba82164
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-06-09 16:29:51 +0530

    setup

--- flow/config.js
+++ flow/config.js
@@ -1,9 +1,18 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "app.detail.title": "Flow Next.js Quick Start",
-  "app.detail.icon": "https://unavatar.io/twitter/muttonia",
-  "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_API,
-  "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET,
-  "0xProfile": process.env.NEXT_PUBLIC_CONTRACT_PROFILE, // The account address where the smart contract lives
-})
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  // We will also specify the network as some of the FCL parts need it to properly do it's work
+  "flow.network": "testnet",
+  // This will be the title of our DApp
+  "app.detail.title": "FlowPay",
+  "app.detail.icon": "https://i.ibb.co/5kv5Lf6/New-Project.png",
+  // Next two will define where Wallet Discovery is located
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "discovery.authn.endpoint":
+    "https://fcl-discovery.onflow.org/api/testnet/authn",
+
+  // We will also set aliases for the contracts we will use in this example
+  "0xFLOW": "0x7e60df042a9c0868",
+  "0xFT": "0x9a0766d93b6608b7",
+});
